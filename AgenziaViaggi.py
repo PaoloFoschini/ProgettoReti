b7415ec7a010ba1c482df8839d85f7c99889e44f
Paolo Foschini <paolo.foschini2@studio.unibo.it>
2025-06-28 15:25:17 +0200

    AgenziaViaggi and Style.css  implemented. Html files changed

--- AgenziaViaggi.py
+++ AgenziaViaggi.py
@@ -1,3 +1,30 @@
 """
 @author: Foschini Paolo
 """
+import sys, signal
+import http.server
+import socketserver
+
+server = socketserver.ThreadingTCPServer(('', 8080), http.server.SimpleHTTPRequestHandler)
+
+server.daemon_threads = True  
+
+server.allow_reuse_address = True  
+
+def signal_handler(signal, frame):
+    print('Exiting http server (Ctrl+C pressed)')
+    try:
+      if (server):
+          server.server_close()
+    finally:
+      sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+  while True:
+    server.serve_forever()
+except KeyboardInterrupt:
+  pass
+
+server.server_close()
